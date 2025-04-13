bbd14f09451bd50b4d07dca6c07b0c83536ab245
metav <naruto.c@tuta.io>
2025-04-13 19:03:56 +0800

    docs: update

--- build/scripts/push-changelog.mjs
+++ build/scripts/push-changelog.mjs
@@ -1,5 +1,4 @@
 import { execSync } from 'child_process';
-// import pkg from '../../package.json' with {type:"json"};
 
 async function main() {
   // https://github.com/eslint/eslint/discussions/15305
